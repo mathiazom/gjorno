48da19db95a81f83dafeaa5732fea36883a4b3eb
TorjusOdegard <torjusodegardd@gmail.com>
2021-02-09 19:57:02 +0100

    Wrote some simple components (8)

---
+++ frontend/src/components/Activity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Activity = () => {
+    return (
+        <div className="aktivtet">
+            <h1>Dette er en aktivitet!</h1>
+            <p>Den starter kl 12:00</p>
+        </div>
+    );
+    }
+
+export default Activity;
---
+++ frontend/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className="tittel">
+            <h1>Her er alle aktivitetene!</h1>
+        </div>
+    );
+}
+
+export default Title;
