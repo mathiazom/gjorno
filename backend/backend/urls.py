e966d7ccff5927f2bde0b784a05edce9fa94752b
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-02-09 17:40:24 +0100

    Defined initial models and api views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from gjorno.views import ActivityView
 
 router = routers.DefaultRouter()
+router.register("activities", ActivityView, 'activities')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/gjorno/admin.py
+++ backend/gjorno/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Activity, Category, Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Category, CategoryAdmin)
