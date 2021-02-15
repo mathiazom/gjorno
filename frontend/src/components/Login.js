865a33e3de766bc005371a013551ebabff8ff041
Olga <olgasa@stud.ntnu.no>
2021-02-15 10:24:01 +0100

    Made login button, missing a node-module file

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="loginButton">
+            <button>
+                Logg inn
+            </button>
+        </div>
+    )
+}
+
+export default Login;
