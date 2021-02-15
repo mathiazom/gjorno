70e8b72c9ace6772cf424e971341984619347ae0
TorjusOdegard <torjusodegardd@gmail.com>
2021-02-15 14:52:32 +0100

    Added the navbar to the frontpage (#8)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 import Activity from './components/Activity';
-import Title from './components/Title';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Title />
-      <Activity />
-      <Activity />
-      <Activity />
       <Activity />
     </div>
   );
