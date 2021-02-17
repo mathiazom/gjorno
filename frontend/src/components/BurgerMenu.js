143df739db94826bdc64fb8563243e37791fede1
Olga <olgasa@stud.ntnu.no>
2021-02-17 19:14:06 +0100

    Temporary burger menu added (#22)

--- frontend/src/components/BurgerMenu.js
+++ frontend/src/components/BurgerMenu.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 const BurgerMenu = () => {
     return(
-        <div>
-            <div className="burgers"></div>
-            <div className="burgers"></div>
-            <div className="burgers"></div>
+        <div id="menuToggle">
+
+            <input type="checkbox" />
+
+            <ul id="menu">
+            <a href="#"><li>Test</li></a>
+            <a href="#"><li>Test</li></a>
+            <a href="#"><li>Test</li></a>
+            <a href="#"><li>Test</li></a>
+            </ul>
+
+            <span className="burgers"></span>
+            <span className="burgers"></span>
+            <span className="burgers"></span>
+
         </div>
     )
 }
