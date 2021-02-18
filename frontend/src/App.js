dcdf43409d8408cb29d9d8fbcf5b7042d8988a73
TorjusOdegard <torjusodegardd@gmail.com>
2021-02-18 13:19:43 +0100

    Collecting data from the server using axios. (#9)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import axios from 'axios'
 
+<<<<<<< HEAD
 export default class App extends React.Component {
 
     constructor(props) {
@@ -46,3 +47,36 @@ export default class App extends React.Component {
         )
     }
 }
+=======
+const axios = require('axios'); //proxy set to "http://localhost:8000" in package.json
+
+export default class App extends React.Component {
+  state = {
+    data: {}
+  }
+  constructor() {
+    super();
+    axios.get('/api/activities/')
+      .then(res => {const data = res.data;
+      this.setState({ data });
+    });
+  }
+
+  returnAllActivities() {
+    // make loop to return a activity for each activity in the data from the server. 
+    // E.g. <Activity data={this.state.data[0 .. n]} />
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <Activity data={this.state.data[0]}/>
+        <Activity data={this.state.data[1]}/>     
+        <Activity data={this.state.data[2]}/> 
+      </div>
+    )
+  }
+}
+  
+>>>>>>> bb6d168 (Collecting data from the server using axios. (#9))
