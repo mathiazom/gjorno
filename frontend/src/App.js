9a47d1b9ce6fd64889acffb1575c271e7860c4d3
TorjusOdegard <torjusodegardd@gmail.com>
2021-02-18 18:46:17 +0100

    Added login form, and made the background blurry. (#10)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import './App.css';
+import './login.css';
 import Activity from './components/Activity';
 import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
+      <input type="checkbox" id="show" />
       <Navbar/>
       <Activity />
       <Activity />
       <Activity />
       <Activity />
       <Activity />
+
+      <LoginForm />
     </div>
   );
 }
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,28 +1,76 @@
 import React from 'react';
 
 function LoginForm() {
+    const addClass = () => {
+        document.getElementById("container").classList.add('right-panel-active');
+    }
+
+    const removeClass = () => {
+        document.getElementById("container").classList.remove('right-panel-active');
+    }
+
+    const closeLogin = () => {
+        document.getElementById("show").checked = false;
+    }
+
+    //const axios = require('axios');
+
+    /*const register = () => {
+        axios.post({
+            "username": "",
+            "email": "",
+            "password1": "",
+            "password2": "",
+        }, "http://localhost:8000/auth/register/"))
+        .then(res => {window.localStorage.setItem("Token", res.data.key)}
+    }*/
+
+    /*const login = () => {
+        axios.post({
+            "username": "",
+            "password": "",
+        }, "http://localhost:8000/auth/login/")
+        .then(res => {window.localStorage.setItem("Token", res.data.key)})
+    }*/
+
     return (
-        <section>
-          <div className="box">
-          <div className="text-center mt-1">
-              <form style="max-width:320px;margin:auto;">
-                  <h1 className="h1 mb-3"> Gjørno</h1>
-                  <h1 className="h6" style="font-weight: normal;">Vennligst logg inn</h1>
-                  <label className="sr-only" htmlFor="brukernavn">Brukernavn
-                  </label>
-                  <input type="text" className="form-control" id="brukernavn" placeholder="Brukernavn" required autoFocus />
-                  <label htmlFor="password" className="sr-only">Passord</label>
-                  <input type="password" id="password" placeholder="Passord" className="form-control" required />
-                  <div className="checkbox mt-3">
-                      <label>
-                          <input type="checkbox" value="remember-me" text="Husk meg"/>
-                      </label>
-                  </div>
-                  <div className="mt-3"><button className="btn btn-lg btn-primary btn-block">Logg inn</button></div>
-              </form>
-          </div>
-          </div>
-      </section>
+        <div className="container" id="container">
+            <a className="close" onClick={closeLogin}></a>
+	        <div className="form-container sign-up-container">
+		        <form action="#">
+			        <h1>Lag konto</h1>
+			        <span>med navn, epost og passord</span>
+			        <input type="text" placeholder="Navn" />
+			        <input type="email" placeholder="Epost" />
+			        <input type="password" placeholder="Passord" />
+			        <button>Registrer deg</button>
+		        </form>
+	        </div>
+	        <div className="form-container sign-in-container">
+		        <form action="#">
+			        <h1>Logg inn</h1>
+                    <span>med brukernavn og passord</span>
+			        <input type="email" placeholder="Brukernavn" required />
+                    <input type="password" placeholder="Passord" required />
+                    <a href="#">Glemt passord?</a>
+                    <button>Logg inn</button>
+                </form>
+            </div>
+            <div className="overlay-container">
+                <div className="overlay">
+                    <div className="overlay-panel overlay-left">
+                        <h1>Velkommen!</h1>
+                        <p>Logg inn for å kunne delta på aktiviteter!</p>
+                        <button className="ghost" id="signIn" onClick={() => { removeClass() }}>Logg inn</button>
+                    </div>
+                    <div className="overlay-panel overlay-right">
+                        <h1>Heisann!</h1>
+                        <p>Registrer deg for å bli med på aktiviteter!</p>
+                        <button className="ghost" id="signUp" onClick={() => { addClass() }}>Registrer deg</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
