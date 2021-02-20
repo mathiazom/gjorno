e38bed14410f4e9de5eb6de78194c8b4df470286
TorjusOdegard <torjusodegardd@gmail.com>
2021-02-20 22:28:45 +0100

    User can register and login (#32)

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,61 +1,76 @@
 import React from 'react';
+import axios from 'axios';
 
-function LoginForm() {
-    const addClass = () => {
+export default class LoginForm extends React.Component {
+    addClass = () => {
         document.getElementById("container").classList.add('right-panel-active');
     }
 
-    const removeClass = () => {
+    removeClass = () => {
         document.getElementById("container").classList.remove('right-panel-active');
     }
 
-    const closeLogin = () => {
+    closeLogin = () => {
         document.getElementById("show").checked = false;
     }
 
-    //const axios = require('axios');
+    handleSubmit = event => {
+        event.preventDefault();
+        const user = { 
+            "username": document.getElementById("reg-username").value,
+            "password1": document.getElementById("reg-password1").value,
+            "password2": document.getElementById("reg-password2").value,
+            "phone_number": "11111111"
+        };
+        console.log(user);
+        axios.post("http://localhost:8000/auth/register/", user)
+            .then(res => { 
+                console.log(res);
+                {window.localStorage.setItem("Token", res.data.key)}
+                this.closeLogin();
+            })
+            .catch(error => {
+                console.log(error.response);
+            })}
+    
 
-    /*const register = () => {
-        axios.post({
-            "username": "",
-            "email": "",
-            "password1": "",
-            "password2": "",
-        }, "http://localhost:8000/auth/register/"))
-        .then(res => {window.localStorage.setItem("Token", res.data.key)}
-    }*/
+    login = () => {
+        axios.post("http://localhost:8000/auth/login/", {
+            "username": document.getElementById("login-username").value,
+            "password": document.getElementById("login-password").value,
+        }).then(res => {
+            window.localStorage.setItem("Token", res.data.key)
+            console.log(res.data.key);
+            this.closeLogin();
+        }).catch(error => {
+            console.log(error.response);
+    })}
 
-    /*const login = () => {
-        axios.post({
-            "username": "",
-            "password": "",
-        }, "http://localhost:8000/auth/login/")
-        .then(res => {window.localStorage.setItem("Token", res.data.key)})
-    }*/
 
-    return (
+    render() {
+        return (
         <div className="container" id="container">
-            <a className="close" onClick={closeLogin}/>
+            <a className="close" onClick={this.closeLogin}/>
             <div className="form-container sign-up-container">
                 <form action="#">
                     <h1>Lag konto</h1>
                     <div className={"mt-3 mb-3"}>
-                        <input className={"rounded"} type="text" placeholder="Brukernavn" />
-                        <input className={"rounded"}  type="password" placeholder="Passord" />
-                        <input className={"rounded"}  type="password" placeholder="Gjenta passord" />
+                        <input id="reg-username" className={"rounded"} type="text" placeholder="Brukernavn" />
+                        <input id="reg-password1" className={"rounded"} type="password" placeholder="Passord" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
+                        <input id="reg-password2" className={"rounded"} type="password" placeholder="Gjenta passord" />
                     </div>
-                    <button>Registrer deg</button>
+                    <button type="submit" onClick={this.handleSubmit}>Registrer deg</button>
                 </form>
             </div>
             <div className="form-container sign-in-container">
                 <form action="#">
                     <h1>Logg inn</h1>
                     <div className={"mt-3"}>
-                        <input className={"rounded"}  type="text" placeholder="Brukernavn" required />
-                        <input className={"rounded"}  type="password" placeholder="Passord" required />
+                        <input id="login-username" className={"rounded"}  type="text" placeholder="Brukernavn" required />
+                        <input id="login-password" className={"rounded"}  type="password" placeholder="Passord" required />
                     </div>
                     <a href="#">Glemt passord?</a>
-                    <button>Logg inn</button>
+                    <button onClick={this.login}>Logg inn</button>
                 </form>
             </div>
             <div className="overlay-container">
@@ -63,17 +78,15 @@ function LoginForm() {
                     <div className="overlay-panel overlay-left">
                         <h1>Velkommen!</h1>
                         <p>Logg inn for å kunne delta på aktiviteter!</p>
-                        <button className="ghost" id="signIn" onClick={() => { removeClass() }}>Logg inn</button>
+                        <button className="ghost" id="signIn" onClick={this.removeClass}>Logg inn</button>
                     </div>
                     <div className="overlay-panel overlay-right">
                         <h1>Heisann!</h1>
                         <p>Registrer deg for å bli med på aktiviteter!</p>
-                        <button className="ghost" id="signUp" onClick={() => { addClass() }}>Registrer deg</button>
+                        <button className="ghost" id="signUp" onClick={this.addClass}>Registrer deg</button>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
-
-export default LoginForm;
+        </div>);
+        }
+    }
