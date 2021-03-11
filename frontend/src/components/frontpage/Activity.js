2767391c2274c061672f78ecf52add8210dce5a3
Olga <olgasa@stud.ntnu.no>
2021-03-11 11:53:36 +0100

    Removed duplicate info from Activity (#46)

--- frontend/src/components/frontpage/Activity.js
+++ frontend/src/components/frontpage/Activity.js
@@ -16,19 +16,17 @@ const Activity = (props) => {
                     <h5 className="card-title text-success">{props.data.title}</h5>
                     <p className="card-text">{props.data.description}</p>
                 </div>
-                <div className={"col-12 col-xl-4 text-end d-none d-xl-block pe-5"}>
+                <div className={"col-12 col-xl-4 text-end d-none d-xl-block pe-5"}> 
                     <div className={"text-secondary"}>
                         <p>Publisert av <span className={"text-success"}><b>{props.data.username}</b></span></p>
                     </div>
-                    <a href="#" className="btn btn-success float-right">Legg til i logg</a>
-                </div>
-                <div className={"col-12 d-block d-xl-none pt-3 pb-3"}>
-                    <div className={"text-secondary"}>
-                        <span>Publisert av <span className={"text-success"}><b>{props.data.username}</b></span></span>
+                    <div>
+                        <a href="#" className="btn btn-success float-right">Legg til i logg</a>
                     </div>
-                    <a href="#" className="btn btn-success mt-3">Legg til i logg</a>
+                    <div>
+                        <Link to={`/activity-details/${props.data.id}`} className={"btn btn-success mt-3"}>Se mer</Link>
+                    </div>
                 </div>
-                <Link to={`/activity-details/${props.data.id}`} className={"btn btn-success w-30"}>Se mer</Link>
             </div>
         </div>
     );
