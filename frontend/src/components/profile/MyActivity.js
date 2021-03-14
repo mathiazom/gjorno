78635636d9f8dba512944963a1da7ad518a8dced
Olga <olgasa@stud.ntnu.no>
2021-03-14 13:51:00 +0100

    Added link to activity details on MyActivity.js (#46)

--- frontend/src/components/profile/MyActivity.js
+++ frontend/src/components/profile/MyActivity.js
@@ -19,7 +19,9 @@ export default class MyActivity extends React.Component {
             <div className="card w-100 mt-4 mb-4 ps-3 pe-3">
                 <div className="card-body d-flex row">
                     <div className={"col pt-2 pb-2"}>
+                    <Link to={`/activity-details/${this.props.data.id}`} style={{textDecoration: "none"}}>
                         <h5 className="card-title text-success">{this.props.data.title}</h5>
+                    </Link>
                         <p className="card-text">{this.props.data.description}</p>
                     </div>
                     <div className={"col-2 d-none d-md-flex justify-content-end align-items-center"}>
