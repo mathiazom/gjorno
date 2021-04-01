ddbf8372d276b8914c0938492600f042a45ef199
Olga <olgasa@stud.ntnu.no>
2021-04-01 14:24:44 +0200

    Fixed month index (#76)

--- frontend/src/components/profile/log/MyLoggedActivity.js
+++ frontend/src/components/profile/log/MyLoggedActivity.js
@@ -18,7 +18,7 @@ export default class MyLoggedActivity extends React.Component {
         const months = ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "november", "desember"]
         let date = new Date(dateString).toISOString().slice(0, 16).replace(/-/g, ".").replace("T", " ")
         let time = date.slice(10,16)
-        date = parseInt(date.slice(8,10)) + " " + months[date.slice(6,7)] + " " + date.slice(0,4)
+        date = parseInt(date.slice(8,10)) + " " + months[date.slice(6,7)-1] + " " + date.slice(0,4)
         return date + " " + time
     }
 
