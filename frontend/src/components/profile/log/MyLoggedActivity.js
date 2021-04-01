72e598c84161a2600014058efa88a7e67eb59467
Olga <olgasa@stud.ntnu.no>
2021-04-01 14:22:35 +0200

    Nicer date format and "upcoming" badge (#76)

--- frontend/src/components/profile/log/MyLoggedActivity.js
+++ frontend/src/components/profile/log/MyLoggedActivity.js
@@ -14,21 +14,27 @@ export default class MyLoggedActivity extends React.Component {
         super(props);
     }
 
+    niceDate(dateString) {
+        const months = ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "november", "desember"]
+        let date = new Date(dateString).toISOString().slice(0, 16).replace(/-/g, ".").replace("T", " ")
+        let time = date.slice(10,16)
+        date = parseInt(date.slice(8,10)) + " " + months[date.slice(6,7)] + " " + date.slice(0,4)
+        return date + " " + time
+    }
+
     render() {
         return (
             <div className="card w-100 mt-4 mb-4 ps-3 pe-3">
                 <div className="card-body d-flex row">
                     <div className={"col pt-2 pb-2"}>
+                            <p className="card-text d-flex text-muted mb-1">{this.props.data.has_registration ? 
+                            this.niceDate(this.props.data.starting_time) : 
+                            this.niceDate(this.props.data.log_timestamp)}
+                            {this.props.data.has_registration && (new Date(this.props.data.starting_time) - Date.now()) > 0 && <span className="badge rounded-pill ms-2 bg-primary align-self-center">Kommende</span>}</p>
                         <Link to={`/activity-details/${this.props.data.id}`} style={{textDecoration: "none"}}>
                             <h5 className="card-title text-success">{this.props.data.title}</h5>
                         </Link>
                         <p className="card-text">{this.props.data.ingress}</p>
-                    </div>
-                    <div className={"col-2 d-none d-md-flex justify-content-end flex-column"}>
-                        {this.props.data.has_registration && (new Date(this.props.data.starting_time) - Date.now()) > 0 && <p className="card-text">Kommende</p>}
-                        <p className="card-text">{this.props.data.has_registration ? 
-                        new Date(this.props.data.starting_time).toISOString().slice(0, 16).replace(/-/g, ".").replace("T", " ") : 
-                        new Date(this.props.data.log_timestamp).toISOString().slice(0, 16).replace(/-/g, ".").replace("T", " ")}</p>
                     </div>
                 </div>
             </div>
