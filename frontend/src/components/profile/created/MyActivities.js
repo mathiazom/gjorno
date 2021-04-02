23275ea805cf5cb5ef46bd097326fcc83b530292
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-04-02 16:36:47 +0200

    Styled no created activities message (#77)

--- frontend/src/components/profile/created/MyActivities.js
+++ frontend/src/components/profile/created/MyActivities.js
@@ -30,8 +30,15 @@ export default class MyActivities extends React.Component {
                 <h2>Mine aktiviteter</h2>
                 <div>
                     {this.renderAllActivities()}
-                    {this.state.data.length == 0 ? <blockquote className="blockquote"><p className="mt-2">Du har ikke opprettet noen aktiviteter. Velg <q>Ny aktivitet</q> i menyen til venstre for å komme i gang.</p></blockquote> : null}
-                    {this.state.data.length <= 3 ? null : <Link title="Vis alle" to={`/profile/created`} className={"btn btn-outline-success w-100 mb-4 ps-3 pe-3"}>Vis alle</Link>}
+                    {this.props.activities.length === 0 &&
+                    <div className={"card"}>
+                        <div className={"card-body p-4 d-flex"}>
+                            <i className="fas fa-info-circle fa-lg text-muted align-self-center me-4"/>
+                            <p className="text-muted fs-6 m-0">Du har ikke opprettet noen aktiviteter enda. <br/>Velg <q>Ny aktivitet</q> i menyen til venstre for å komme i gang.</p>
+                        </div>
+                    </div>
+                    }
+                    {this.props.activities.length <= 3 ? null : <Link title="Vis alle" to={`/profile/created`} className={"btn btn-outline-success w-100 mb-4 ps-3 pe-3"}>Vis alle</Link>}
                 </div>
             </div>
         );
