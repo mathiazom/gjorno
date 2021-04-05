0445339f2b3a3b5180a00d08084c1549d61956e7
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-04-05 22:01:31 +0200

    Fixed activity log link on profile (#89)

--- frontend/src/components/profile/log/MyLoggedActivity.js
+++ frontend/src/components/profile/log/MyLoggedActivity.js
@@ -7,8 +7,8 @@ export default class MyLoggedActivity extends React.Component {
     /**
      * We take in some props (title, ingress and date) to make the logged Activity.
      * This activity is the one stored on the users profile.
-     * 
-     * @param {*} props 
+     *
+     * @param {*} props
      // */
     constructor(props) {
         super(props);
@@ -32,11 +32,11 @@ export default class MyLoggedActivity extends React.Component {
             <div className="card w-100 mt-4 mb-4 ps-3 pe-3">
                 <div className="card-body d-flex row">
                     <div className={"col pt-2 pb-2"}>
-                            <p className="card-text d-flex text-muted mb-1">{this.props.data.has_registration ? 
-                            this.niceDate(this.props.data.starting_time) : 
+                            <p className="card-text d-flex text-muted mb-1">{this.props.data.has_registration ?
+                            this.niceDate(this.props.data.starting_time) :
                             this.niceDate(this.props.data.log_timestamp)}
                             {this.props.data.has_registration && (new Date(this.props.data.starting_time) - Date.now()) > 0 && <span className="badge rounded-pill ms-2 bg-primary align-self-center">Kommende</span>}</p>
-                        <Link to={`/activity-details/${this.props.data.id}`} className={"no-decoration"}>
+                        <Link to={`/activity-details/${this.props.data.activity}`} className={"no-decoration"}>
                             <h5 className="card-title text-success">{this.props.data.title}</h5>
                         </Link>
                         <p className="card-text">{this.props.data.ingress}</p>
