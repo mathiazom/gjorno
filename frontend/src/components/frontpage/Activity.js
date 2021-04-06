dbe3c0d819a3e28e1421984ddbbc4afb04a103b0
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-04-06 20:14:21 +0200

    Added organization icon to profile and activity details page

--- frontend/src/components/frontpage/Activity.js
+++ frontend/src/components/frontpage/Activity.js
@@ -93,11 +93,12 @@ const Activity = (props) => {
                 <div className={"col-12 col-xl-4 text-end d-none d-xl-block pe-5"}>
                     <div className={"text-secondary"}>
                         <span>Publisert av</span><br/>
-                        <span className={"text-success"}><b>{props.activity.username} </b>
+                        <span className={"text-success d-flex justify-content-end"}><b>{props.activity.username} </b>
                         {/* Verified icon for organizations */}
                             {props.activity.is_organization &&
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-patch-check-fill text-primary" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-patch-check-fill text-primary ms-1 align-self-center" viewBox="0 0 16 16">
                                     <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+                                    <title>Organisasjon</title>
                                 </svg>
                             }
                         </span>
