c4bf095182a8c14b7fe88607f24d477586affbb8
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2021-04-08 10:08:31 +0200

    Fixed verified icon for long usernames

--- frontend/src/components/activitydetails/ActivityHost.js
+++ frontend/src/components/activitydetails/ActivityHost.js
@@ -13,10 +13,13 @@ export default class ActivityHost extends React.Component {
                 <img className="card-img-top img-fluid profile-image"
                      src={this.props.author?.avatar || "/images/profil.png"} alt="profile"/>
                 <div className="card-body">
-                    <h4 className="card-title d-flex">{this.props.author?.username}
+                    <h4 className="card-title d-flex text-break">{this.props.author?.username}
                         {/* Verified icon for organizations */}
                         {this.props.author?.is_organization &&
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-patch-check-fill text-primary ms-1 align-self-center" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-patch-check-fill text-primary ms-1 align-self-center" viewBox="0 0 16 16"
+                             style={{minWidth: "16px"}}
+                        >
                             <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
                             <title>Organisasjon</title>
                         </svg>
